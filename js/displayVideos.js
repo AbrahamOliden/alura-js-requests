679495fe4a380a260bf2e14d32a6f5ae8d81f032
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-07 00:37:19 -0600

    function to create li elements and to consume local API data

---
+++ js/displayVideos.js
@@ -0,0 +1,26 @@
+import { APIConection } from "./APIConnection.js";
+
+const list = document.querySelector('[data-list]');
+
+function createCard(titulo, descripcion, url, imagen) {
+    const video = document.createElement('li');
+    video.className = 'videos__item';
+    video.innerHTML = `
+    <iframe width="100%" height="75%" src="${url}" title="${titulo}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
+    <div class="descripcion-video">
+        <img src="${imagen}" alt="">
+        <h3>${titulo}</h3>
+        <p>${descripcion}</p>
+    </div>
+    `;
+
+    return video;
+};
+
+async function listVideos() {
+    const APIList = await APIConection.listVideos();
+
+    APIList.forEach( video => list.appendChild( createCard(video.titulo, video.descripcion, video.url, video.imagen) ) );
+};
+
+listVideos();
