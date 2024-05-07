cb6e9d5d668a101808f31c4320f67185b3e40a86
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-06 23:35:47 -0600

    parsed connection and exported APIConnection const

--- js/APIConnection.js
+++ js/APIConnection.js
@@ -1,7 +1,14 @@
 async function listVideos() {
     const connection = await fetch("http://localhost:3001/videos");
 
-    console.log(connection);
+    const parsedConnection = connection.json();
+
+    // console.log(parsedConnection);
+    return parsedConnection;
 };
 
-listVideos();
+export const APIConection = {
+    listVideos
+};
+
+// listVideos();
