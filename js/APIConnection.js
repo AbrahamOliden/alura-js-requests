1a851416a77ff93adb74dcbe3238597793871647
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-07 23:18:35 -0600

    implemented uploadVideo function

--- js/APIConnection.js
+++ js/APIConnection.js
@@ -1,14 +1,29 @@
 async function listVideos() {
     const connection = await fetch("http://localhost:3001/videos");
-
     const parsedConnection = connection.json();
-
     // console.log(parsedConnection);
     return parsedConnection;
 };
 
+async function uploadVideo(title, description, url, image) {
+    const connection = await fetch("http://localhost:3001/videos", {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({
+            title: title,
+            description: description,
+            url: url,
+            image: image
+        })
+    });
+
+    const parsedConnection = connection.json();
+
+    return parsedConnection;
+};
+
 export const APIConection = {
-    listVideos
+    listVideos, uploadVideo
 };
 
 // listVideos();
