882d2c840b10823dace509871b403173805f414b
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-09 00:28:52 -0600

    imported function createCard adds a child to index.html data-list

---
+++ js/filterVideos.js
@@ -0,0 +1,19 @@
+import { APIConnection } from './APIConnection.js';
+import createCard from './displayVideos.js';
+
+async function filterVideo(event) {
+    event.preventDefault();
+
+    const lookUpData = document.querySelector('[data-search]').value;
+    const lookUp = await APIConnection.lookUpVideos(lookUpData);
+
+    const list = document.querySelector('[data-list]');
+
+    lookUp.forEach( video => list.appendChild( createCard(video.titulo, video.descripcion, video.url, video.imagen) ) );
+
+    // console.log(lookUp);
+};
+
+const button = document.querySelector('[data-search-button]');
+
+button.addEventListener('click', (event) => filterVideo(event) );
